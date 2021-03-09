6d6c31ba9204067b3447c53f45530c420e880e13
Saleh Alhaddad <salehalhaddad010@gmail.com>
2021-03-09 23:52:11 +0300

    Create scraper.rb
    What is Scraping in Ruby?
    Scraping in Ruby is a way to extract data from web pages so you can use it in your program. We usually scrape data when the website doesn’t provide raw data, APIs for instance, for the information we need. So we just pull the web page to our program and extract some raw data from it.
    
    Introducing Nokogiri
    The word “Nokogiri” is a Japanese saw, a tool that is used in woodworking. It is also a Ruby gem that is used for data scraping ;-). To install this gem on your machine use the following command in your terminal:
    
    gem install nokogiri

---
+++ scraper.rb
@@ -0,0 +1,13 @@
+require 'nokogiri'
+require 'open-uri'
+class MovieScraper
+   url = "https://editorial.rottentomatoes.com/guide/essential-
+          movies-to-watch-now/"
+   html = open(url)
+   doc = Nokogiri::HTML(html)
+   movies = doc.css("div .row.countdown-item")
+   movies_names = []
+   movies.each_with_index do |movie, idx|
+     movies_names << movie.css("div h2 a").text.strip
+   end
+end
